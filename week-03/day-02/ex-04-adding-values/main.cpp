4aca924fe8237c96fa1a5a2a191155300e78580c
nmarton818 <noreply818@gmail.com>
2018-11-06 12:52:06 +0100

    ex-04 adding values

---
+++ week-03/day-02/ex-04-adding-values/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+int main() {
+    // Add two numbers using pointers
+
+    int a = 20;
+    int b = 17;
+    int* aPtr = &a;
+    int* bPtr = &b;
+
+    std::cout << *aPtr + (*bPtr) << std::endl;
+
+
+
+    return 0;
+}
