86abe1a918bc8028f105a5f40565caca15676d39
nmarton818 <noreply818@gmail.com>
2018-12-05 12:40:05 +0100

    ex-03 - introduction

---
+++ week-07/day-03/ex-03-introduction/main.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+int main(){
+    // Write a program that prints a few details to the terminal window about you
+    // It should print each detail to a new line.
+    //  - Your name
+    //  - Your age
+    //  - Your height in meters (as a decimal fraction)
+    //
+    //  Use variables to store the informations!
+    //
+    //  Example output:
+    //  John Doe
+    //  31
+    //  1.87
+
+
+    printf("My name is Marton Nagy.\n");
+    printf("I am %d years old.\n", 24);
+    printf("I am %.2f meters high.\n", 1.75);
+
+    return 0;
+}
