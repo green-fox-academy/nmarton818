a269328b9a862231c0613377fa050fa4424856e1
nmarton818 <noreply818@gmail.com>
2018-12-05 12:35:19 +0100

    ex-02 - hello others

--- week-07/day-03/ex-01-hello-me/main.c
+++ week-07/day-03/ex-01-hello-me/main.c
@@ -1,7 +1,7 @@
 #include <stdio.h>
 
 int main() {
-    
+
     // Modify this program to greet you instead of the World!
     printf("Hello, Marci!\n");
     return 0;
---
+++ week-07/day-03/ex-02-hello-others/main.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main() {
+    // Greet 3 of your classmates with this program, in three separate lines
+    // like:
+    //
+    // Hello, Esther!
+    // Hello, Mary!
+    // Hello, Joe!
+
+    printf("Hello, David!\n");
+    printf("Hello, Zsuzsi!\n");
+    printf("Hello, Fanni!\n");
+    return 0;
+}
