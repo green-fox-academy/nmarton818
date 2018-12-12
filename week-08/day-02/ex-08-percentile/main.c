9fff0c6796f153efb93b927377ec5b2d04a1a987
nmarton818 <noreply818@gmail.com>
2018-12-12 13:11:55 +0100

    ex-08 - percentile finished

--- week-08/day-02/ex-08-percentile/main.c
+++ week-08/day-02/ex-08-percentile/main.c
@@ -25,17 +25,20 @@ int* matrix2vector(int** matrix, int x, int y){
 
 void vector_sort(int* vector, int size){
     for(int i = 0; i < size - 1; i++){
-        for(int j = i; j < size - 1; j++)
-            if(vector[j] < vector[j+1])
+        for(int j = 0; j < size - 1; j++)
+            if(vector[j] > vector[j+1])
                 swap(&vector[j], &vector[j+1]);
     }
 }
 int percentile(int* vector, int size, int percentile){
-    if(percentile > 100)
-        return vector[size];
+    float perc = (float) percentile;
+    if(percentile >= 100)
+        return vector[size-1];
     if(percentile < 0)
         return -1;
-    else;
+    else{
+        return vector[(int)((float) size * (perc/100))];
+    }
 }
 
 int main() {
@@ -44,17 +47,17 @@ int main() {
     //Create a function which can calculate the n-th percentile of the elements.
     //Calculate the 80-th and the 90-th percentile.
     int x, y;
-    printf("X: \n");
+    printf("X:\n");
     scanf("%d", &x);
-    printf("Y: \n");
+    printf("Y:\n");
     scanf("%d", &y);
     int** matrix = (int**) malloc(x * sizeof(int*));
     for(int i = 0; i < x; i++){
         matrix[i] = (int*) calloc(y, sizeof(int));
     }
 
-    for(int i = 0; i < y; i++){
-        for(int j = 0; j < x; j++){
+    for(int i = 0; i < x; i++){
+        for(int j = 0; j < y; j++){
             matrix[i][j] = i + j;
             printf("%d ", matrix[i][j]);
         }
@@ -64,6 +67,19 @@ int main() {
     for(int i = 0; i < x * y; i++){
         printf("%d ", vector[i]);
     }
+
+    int perc = percentile(vector, x*y, 10);
+    printf("\n10 percentile: %d\n", perc);
+    perc = percentile(vector, x*y, 20);
+    printf("20 percentile: %d\n", perc);
+    perc = percentile(vector, x*y, 30);
+    printf("30 percentile: %d\n", perc);
+    perc = percentile(vector, x*y, 40);
+    printf("40 percentile: %d\n", perc);
+    perc = percentile(vector, x*y, 50);
+    printf("50 percentile: %d\n", perc);
+    perc = percentile(vector, x*y, 60);
+    printf("60 percentile: %d\n", perc);
 
     return 0;
 }
